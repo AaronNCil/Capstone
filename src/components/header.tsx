37e2d0b444530b0ebe8f313c0939aae2b5441ece
Aaron Cil <cilaaron27@gmail.com>
2023-02-01 11:16:36 -0500

    fixed sign up

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -98,10 +98,10 @@ export default function Header() {
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                   >
-                   <div>
+                   {/* <div>
                    {isLoggedIn ? (
                    <div>Welcome Back!</div>
-                 ) : (
+                 ) : ( */}
                     <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                       <Menu.Item>
                         {({ active }) => (
@@ -124,7 +124,7 @@ export default function Header() {
                         )}
                       </Menu.Item>
                     </Menu.Items>
-                 )}</div>
+                 {/* )}</div> */}
                   </Transition>
                 </Menu>
               </div>
